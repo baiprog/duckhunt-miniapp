096fc53bc49dbed97df79546ec1c933b05607ba2
root <root@vm3270809.firstbyte.club>
2025-04-07 01:03:09 +0300

    fire

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsxkESoJ2_fTMmgM6YJuPi4QW7iHv43Go",
-  authDomain: "duckhuntweb3-9db08.firebaseapp.com",
-  projectId: "duckhuntweb3-9db08",
-  storageBucket: "duckhuntweb3-9db08.firebasestorage.app",
-  messagingSenderId: "410131966059",
-  appId: "1:410131966059:web:3d4c2b30304eae3ac0692e"
+  apiKey: "AIzaSyAumHgwZ3xkUSAXaD8dSo9_Vw6YX8ziDQM",
+  authDomain: "duckhuntweb3.firebaseapp.com",
+  projectId: "duckhuntweb3",
+  storageBucket: "duckhuntweb3.firebasestorage.app",
+  messagingSenderId: "1048255595395",
+  appId: "1:1048255595395:web:3e58ff61ffd45903df2cb6",
+  measurementId: "G-458718Q972"
 };
 
 const app = initializeApp(firebaseConfig);
